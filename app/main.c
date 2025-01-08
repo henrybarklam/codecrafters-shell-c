522c8d3177c4342e64c191d828bf9541c96e0bfd
Henry Barklam <henry@optoinvest.com>
2025-01-08 11:29:07 +0000

    A little bit more documented

--- app/main.c
+++ app/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+// Compiler will always look for this to determine where to start
+int main(){
+  //Flush after every printf
+  // void setbuf( FILE *stream, char *buffer)
+  // sets the internal buffer to use for stream operations
+  // If buffer is null it turns off buffering
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
+  setbuf(stout, NULL);
+  printf("$ "); //This is what the terminal will use as its prompt
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+  //Wait for user input
+  char input[100]; //Max len 100 characters
+  //Reads limited number of characters from a file stream source into an array
+  // of characters
+  //char * fgets(char *string, int length, FILE * stream)
+  fgets(input, 100, stdin); 
   return 0;
 }
