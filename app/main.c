81695798f855b7b46847b8909466e63edcf7af2f
Henry Barklam <henry@optoinvest.com>
2025-01-08 11:42:52 +0000

    Error message for missing command

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Compiler will always look for this to determine where to start
 int main(){
@@ -8,13 +9,13 @@ int main(){
   // If buffer is null it turns off buffering
 
   setbuf(stdout, NULL);
-  printf("$ "); //This is what the terminal will use as its prompt
+  printf("$ ");
 
   //Wait for user input
-  char input[100]; //Max len 100 characters
-  //Reads limited number of characters from a file stream source into an array
-  // of characters
+  char input[100];
   //char * fgets(char *string, int length, FILE * stream)
   fgets(input, 100, stdin); 
+  input[strlen(input) -1] = '\0'; // \0 is the NULL character, end of a string
+  printf("%s: command not found", input); // Breaks for all rn
   return 0;
 }
