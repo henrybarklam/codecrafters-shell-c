9591cbc8e37346de9b5c6671e973ab0cd3f38319
henrybarklam <72357080+henrybarklam@users.noreply.github.com>
2025-01-24 20:24:32 -0500

    feat: type

--- app/main.c
+++ app/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+// Functions need to be declared before they're called
+int contains_string(const char *str, const char *set[], size_t set_size){
+  for (size_t i = 0; i < set_size; i++){
+    if (strcmp(str, set[i]) == 0){
+      return 1;
+    }
+  }
+  return 0;
+}
 // Compiler will always look for this to determine where to start
 int main(){
   //Flush after every printf
@@ -8,8 +17,11 @@ int main(){
   // sets the internal buffer to use for stream operations
   // If buffer is null it turns off buffering
 
+  char* TYPE_COMMAND = "type";  
   char* EXIT_COMMAND = "exit";
   char* ECHO_COMMAND = "echo";
+
+  char *word_set[] = {TYPE_COMMAND, EXIT_COMMAND, ECHO_COMMAND};
   setbuf(stdout, NULL);
   printf("$ ");
 
@@ -20,7 +32,8 @@ int main(){
   const char delim[] = " ";
 
   char* token = strtok(input, delim); //This only gets the first word, need more 
-  // String parsing here to get arguments
+
+  // Should probably start separating these things into different functions
 
   if (strcmp(EXIT_COMMAND, token) == 0){
     return 0;
@@ -38,9 +51,20 @@ int main(){
     }
     printf("%s\n", output);
   }
+  else if (strcmp(TYPE_COMMAND, token) == 0){
+    token = strtok(NULL," ");
+    if(contains_string(token, word_set, sizeof(word_set)/sizeof(word_set[0]))){
+      printf("%s is a shell builtin\n", token);
+    }
+    else{
+      printf("%s: not found\n", token);  
+    }
+
+  }
   else{
     printf("%s: command not found\n", token);
   }
   main();
   return 0;
 }
+
